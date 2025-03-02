6d5bddd4ae7ae5e2dc6e34f275e9337411919fd9
Maxwell Githinji <muriumaxwell1@gmail.com>
2025-03-02 22:47:12 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+if (main) {
+    main.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion"; // Replace 'YOUR-NAME' with your actual name
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
